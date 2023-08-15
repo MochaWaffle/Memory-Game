63fc6b095f038bb0ba6cad90f9107459414b2712
MochaWaffle <vsaini23@medford.k12.ma.us>
2023-08-15 18:18:01 -0400

    start button and progressive levels
    The memory game start button now displays which level you are on when you start the game, and you cannot reset the game, the game resets when you die. If you pressed the wrong button, you can't press other buttons. I still need to fix the bug where if you got the right buttons you can click on another one which will give count as getting it wrong

--- script.js
+++ script.js
@@ -9,13 +9,21 @@ document.addEventListener("DOMContentLoaded", function () {
     let playerIndex = 0;
     let currentLevel = 1;
     let lives = 1; // Change this to 3 after unlocking
+    let gameEnd = false;
+    
+    startButton.addEventListener("click", function () {
+        if (startButton.textContent === "Start") {
+            startGame();
+        }
+    });
 
-    startButton.addEventListener("click", startGame);
+    resetButton.addEventListener("click", resetGame);
 
     function startGame() {
         resetGame();
         generateRandomSequence();
         playSequence();
+        startButton.textContent = "Level " + currentLevel;
     }
 
     function resetGame() {
@@ -28,14 +36,10 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function generateRandomSequence() {
-        const randomIndices = [];
         const squaresToClick = currentLevel + 2; // Adjust the number of squares to click
-        while (randomIndices.length < squaresToClick) {
+        for (let i = 0; i < squaresToClick; i++) {
             const randomIndex = Math.floor(Math.random() * columns.length);
-            if (!randomIndices.includes(randomIndex)) {
-                randomIndices.push(randomIndex);
-                sequence.push(randomIndex);
-            }
+            sequence.push(randomIndex);
         }
     }
 
@@ -43,39 +47,62 @@ document.addEventListener("DOMContentLoaded", function () {
         let index = 0;
         const interval = setInterval(() => {
             const columnIndex = sequence[index];
+            columns.forEach(col => col.classList.remove("glow"));
             columns[columnIndex].classList.add("glow");
             setTimeout(() => {
                 columns[columnIndex].classList.remove("glow");
                 index++;
                 if (index === sequence.length) {
                     clearInterval(interval);
-                    allowPlayerInput();
+                    setTimeout(() => {
+                        allowPlayerInput();
+                    }, 500); // Add a delay before allowing player input
                 }
-            }, 500); // Adjust the duration as needed
+            }, 500); // Adjust the interval as needed
         }, 1000); // Adjust the interval as needed
     }
 
     function allowPlayerInput() {
+        gameEnd = false; // Allow player input
         columns.forEach(col => col.addEventListener("click", handleClick));
     }
 
     function handleClick() {
+        if (gameEnd) return; // Prevent clicking when game has ended
         const clickedIndex = Array.from(columns).indexOf(this);
         if (clickedIndex === sequence[playerIndex]) {
-            this.classList.add("glow");
-            playerIndex++;
-            if (playerIndex === sequence.length) {
-                console.log("resetted");
-                setTimeout(startGame, 1000);
-                
-            }
+            this.classList.add("glow"); // Add glow on correct click
+            setTimeout(() => {
+                this.classList.remove("glow"); // Remove the glow after a short delay
+                playerIndex++;
+                if (playerIndex === sequence.length) {
+                    currentLevel++;
+                    if (currentLevel > 5) {
+                        // Player wins!
+                        alert("Congratulations! You won!");
+                    } else {
+                        startGame();
+                    }
+                }
+            }, 300); // Adjust the delay as needed
         } else {
+            gameEnd = true; // Block further input
             this.classList.add("error");
             setTimeout(() => {
-                resetGame();
+                if (lives > 1) {
+                    lives--;
+                    resetGame();
+                    gameEnd = false; // Allow player input after reset
+                } else {
+                    alert("Game over! You ran out of lives.");
+                    resetGame();
+                    currentLevel = 1;
+                    startButton.textContent = "Start";
+                }
             }, 1000);
         }
     }
+
     // Handle mode buttons
     modeButtons.forEach(button => {
         button.addEventListener("click", function () {
